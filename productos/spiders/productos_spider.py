a6fdea6851fabdfa0a803ecd8bb43a4745690796
robincoello <robincoello@localhost.localdomain>
2016-07-07 18:40:47 +0200

    page principal

--- productos/spiders/productos_spider.py
+++ productos/spiders/productos_spider.py
@@ -3,15 +3,23 @@ import scrapy
 class ProductosSpider(scrapy.Spider):
     name = "productos"
     allowed_domains = ["nanotechproject.org"]
+    
+    
     start_urls = [
         "http://www.nanotechproject.org/cpi/products/"
     ]
 
     def parse(self, response):
         filename = 'pro.txt'
-        scraped = response.xpath("//h5/text()").extract()
-        f = open(filename, 'rw+')
-        f.seek(0, 2)
-        f.writelines(scraped)
-        f.writelines('\n')
-        f.close()
+        scraped = response.xpath("//h5/a").extract()
+        print scraped
+        print '----------------------------------------------'
+        file = open(filename, 'rw+')  
+        for sel in response.xpath('//h5'):
+			title = sel.xpath('a/text()').extract()
+			link = sel.xpath('a/@href').extract()
+			desc = sel.xpath('text()').extract()
+			print title, link, desc       
+			file.writelines(link)	
+			file.writelines('\n')			   	
+        file.close()
