17e2667cae21dbf100f64395de5d3769a62029cb
robincoello <robincoello@localhost.localdomain>
2016-07-07 20:01:53 +0200

    add full path

--- productos/spiders/productos_spider.py
+++ productos/spiders/productos_spider.py
@@ -2,16 +2,19 @@ import scrapy
 
 class ProductosSpider(scrapy.Spider):    
     name = "productos"
-    allowed_domains = ["nanotechproject.org"]  		
+    allowed_domains = ["nanotechproject.org"]
+    
+     		
     start_urls = [
         "http://www.nanotechproject.org/cpi/products/",        
     ]
-    for x in range(1,75):
+    for x in range(1,76):
 		print x
 		start_urls.append("http://www.nanotechproject.org/cpi/products/page" + str(x));  
 		  
 
     def parse(self, response):
+	sitio = 'http://www.nanotechproject.org'
         filename = 'pro.txt'
         print '----------------------------------------------'
         file = open(filename, 'rw+')  
@@ -21,6 +24,7 @@ class ProductosSpider(scrapy.Spider):
 			link = sel.xpath('a/@href').extract()
 			#desc = sel.xpath('text()').extract()			
 			print link  			
+			file.writelines(sitio)				
 			file.writelines(link)				
 			file.writelines('\n')					  
 	file.close()
